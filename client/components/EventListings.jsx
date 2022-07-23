bedddf370eb564d318584aabdba1d5a5f2fb64a0
Vincent Tong <vincent.khang.tong@gmail.com>
2022-07-22 20:01:53 -0500

    Revert "filterby categories work in progress (#94)"
    This reverts commit fd1561395007243d662b4ffc8846337aed9bb988.

--- client/components/EventListings.jsx
+++ client/components/EventListings.jsx
@@ -16,9 +16,6 @@ const EventListings = (props) => {
   let [searchParams, setSearchParams] = useSearchParams();
   const param = searchParams.get("user");
   console.log(param);
-const getFilteredEvents = () => {
-    axios.get()
-}
   
 const getAllEvents = () => {
       axios.get('/api/eventListings')
